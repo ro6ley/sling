0e53d7f77d98493e91c02ff83beb74db24596ab5
Robley <robleyadrian@gmail.com>
2018-07-22 21:48:24 +0300

    chore: update postgres credentials.

--- api/config/test.exs
+++ api/config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :sling, Sling.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "robley",
+  password: "",
   database: "sling_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
